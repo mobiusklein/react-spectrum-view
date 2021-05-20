1fed6bdace8e525037b4438a70d46e1e592c9ac3
Joshua Klein <mobiusklein@gmail.com>
2021-05-19 20:50:00 -0400

    implement CI

--- src/lib.js
+++ src/lib.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom";
 // import { SpectrumViewer } from "./viewer";
 import SpectrumCanvasComponent from "./spectrum/component";
 import { convertScanToLayers } from "./spectrum/utils";
+import { SpectrumData, DataLayer, LineArtist, ProfileLayer, PointLayer,
+         LabeledPeakLayer, DeconvolutedLayer, PrecursorPeakLayer, IsolationWindowLayer,
+  } from "./spectrum/spectrum_layers"
+import * as App from "./viewer"
 
-export function renderSpectrum(container, spectrumData) {
-  let spectrumDataConverted = convertScanToLayers(spectrumData);
+export function renderSpectrum(container, spectrumData, converter) {
+  if (converter === undefined) {
+    converter = convertScanToLayers
+  }
+  let spectrumDataConverted = renderSpectrum(spectrumData);
   let component = (
     <SpectrumCanvasComponent spectrumData={spectrumDataConverted} config={{}} />
   );
@@ -13,4 +20,6 @@ export function renderSpectrum(container, spectrumData) {
   return component;
 }
 
-export { SpectrumCanvasComponent, convertScanToLayers };
+export {
+  SpectrumCanvasComponent, convertScanToLayers, SpectrumData, DataLayer, LineArtist, ProfileLayer, PointLayer,
+  LabeledPeakLayer, DeconvolutedLayer, PrecursorPeakLayer, IsolationWindowLayer, App};
