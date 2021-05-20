df85bd931cc29cee86c8cf293043bdb6876403d3
Joshua Klein <mobiusklein@gmail.com>
2021-05-20 10:07:22 -0400

    Don't bundle the whole viewer application, just the library

--- src/lib.js
+++ src/lib.js
@@ -1,12 +1,18 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-// import { SpectrumViewer } from "./viewer";
 import SpectrumCanvasComponent from "./spectrum/component";
 import { convertScanToLayers } from "./spectrum/utils";
-import { SpectrumData, DataLayer, LineArtist, ProfileLayer, PointLayer,
-         LabeledPeakLayer, DeconvolutedLayer, PrecursorPeakLayer, IsolationWindowLayer,
-  } from "./spectrum/spectrum_layers"
-import * as App from "./viewer"
+import {
+  SpectrumData,
+  DataLayer,
+  LineArtist,
+  ProfileLayer,
+  PointLayer,
+  LabeledPeakLayer,
+  DeconvolutedLayer,
+  PrecursorPeakLayer,
+  IsolationWindowLayer,
+} from "./spectrum/spectrum_layers"
 
 export function renderSpectrum(container, spectrumData, converter) {
   if (converter === undefined) {
@@ -21,5 +27,15 @@ export function renderSpectrum(container, spectrumData, converter) {
 }
 
 export {
-  SpectrumCanvasComponent, convertScanToLayers, SpectrumData, DataLayer, LineArtist, ProfileLayer, PointLayer,
-  LabeledPeakLayer, DeconvolutedLayer, PrecursorPeakLayer, IsolationWindowLayer, App};
+  SpectrumCanvasComponent,
+  convertScanToLayers,
+  SpectrumData,
+  DataLayer,
+  LineArtist,
+  ProfileLayer,
+  PointLayer,
+  LabeledPeakLayer,
+  DeconvolutedLayer,
+  PrecursorPeakLayer,
+  IsolationWindowLayer
+};
